f28d990c38df1cfd2ada7a7aa40935228e9113f7
Akash R <akash.r@thehindu.co.in>
2022-07-10 17:07:17 +0530

    Added global styles

--- lib/src/theme/colors.dart
+++ lib/src/theme/colors.dart
@@ -1,6 +1,9 @@
 import 'dart:ui';
 
-const Color initialBgColor = Color(0XFF1d1f2a);
+const Color primaryDark = Color(0XFF1D1F2A);
+const Color primaryLight = Color(0XFF252b39);
+
+// LetterContainer
 const Color notInWordBgColor = Color(0XFF434952);
 const Color inWrongPlaceBgColor = Color(0XFFCC972D);
 const Color inCorrectPlaceBgColor = Color(0XFF1B7626);
---
+++ lib/src/theme/styles.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final TextStyle letterContainerTextStyle = GoogleFonts.getFont(
+  'Solway',
+  textStyle: const TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 40,
+    color: Colors.white,
+  ),
+);
+
+const EdgeInsets defaultPadding = EdgeInsets.all(15);
+const BorderRadius defaultRadius = BorderRadius.all(Radius.circular(20));
