b00d2bddeef4f30736ff0b1d2b4c34ad290a2205
Akash R <akash.r@thehindu.co.in>
2022-07-10 20:29:47 +0530

    beta version implemented

--- lib/src/views/game_view.dart
+++ lib/src/views/game_view.dart
@@ -16,12 +16,11 @@ class GameView extends StatefulWidget {
 }
 
 class _GameViewState extends State<GameView> {
-
-  // By default, the game would have a 5 letter word, 
+  // By default, the game would have a 5 letter word,
   // to be found out within 6 max guesses.
 
   // So, create 6 Words (6 max guesses),
-  final List<Word> _words = List.generate(6, (_) {
+  List<Word> _words = List.generate(6, (_) {
     return Word(
       // with 5 letters each...
       letters: List.generate(5, (_) {
@@ -31,6 +30,7 @@ class _GameViewState extends State<GameView> {
   });
 
   int currentWordIndex = 0;
+  Word solution = Word.fromString("hello");
 
   // On pressing any alphabet in the keyboard, add that letter to word
   _onKeyPressed(String letter) {
@@ -45,6 +45,81 @@ class _GameViewState extends State<GameView> {
     Word currentWord = _words[currentWordIndex];
     setState(() {
       currentWord.removeLetter();
+    });
+  }
+
+  // On pressing ENTER key, submit the solution
+  _onEnterKeyPressed() {
+    Word currentWord = _words[currentWordIndex];
+    setState(() {
+      if (_words.length - 1 >= currentWordIndex) {
+        for (var i = 0; i < currentWord.letters.length; i++) {
+          Letter currentLetter = currentWord.letters[i];
+          if (solution.letters[i] == currentLetter) {
+            currentWord.letters[i] = currentLetter.copywith(
+              null,
+              LetterStatus.inCorrectPlace,
+            );
+          } else if (solution.letters.contains(currentLetter)) {
+            currentWord.letters[i] = currentLetter.copywith(
+              null,
+              LetterStatus.inWrongPlace,
+            );
+          } else {
+            currentWord.letters[i] = currentLetter.copywith(
+              null,
+              LetterStatus.notInWord,
+            );
+          }
+        }
+      }
+      if (_words.length - 1 == currentWordIndex) {
+        String solutionWord = solution.wordString;
+        bool hasWon = currentWord == solution;
+        showDialog(
+          builder: (BuildContext newContext) {
+            return Scaffold(
+              backgroundColor: Colors.transparent,
+              body: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    !hasWon
+                        ? Text(
+                            "You Lost!. The word was $solutionWord",
+                            style: const TextStyle(
+                              color: Colors.white,
+                            ),
+                          )
+                        : const Text("You Won"),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(newContext).pop();
+                        setState(() {
+                          currentWordIndex = 0;
+                          _words = List.generate(6, (_) {
+                            return Word(
+                              // with 5 letters each...
+                              letters: List.generate(5, (_) {
+                                return Letter.empty();
+                              }),
+                            );
+                          });
+                        });
+                      },
+                      child: Text("Restart"),
+                    )
+                  ],
+                ),
+              ),
+            );
+          },
+          context: context,
+          barrierDismissible: true,
+        );
+      } else {
+        currentWordIndex++;
+      }
     });
   }
 
@@ -68,7 +143,7 @@ class _GameViewState extends State<GameView> {
                 const SizedBox(height: 20),
                 Keyboard(
                   onDeletePress: _onDeleteKeyPressed,
-                  onEnterPress: () {},
+                  onEnterPress: _onEnterKeyPressed,
                   onKeyPress: _onKeyPressed,
                 ),
               ],
