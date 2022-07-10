ef098eafc1b3ed762671cc482b627180b9dc80dd
Akash R <akash.r@thehindu.co.in>
2022-07-10 16:00:00 +0530

    Added keyboard component

---
+++ lib/src/theme/colors.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+const Color initialBgColor = Color(0XFF1d1f2a);
+const Color notInWordBgColor = Color(0XFF434952);
+const Color inWrongPlaceBgColor = Color(0XFFCC972D);
+const Color inCorrectPlaceBgColor = Color(0XFF1B7626);
